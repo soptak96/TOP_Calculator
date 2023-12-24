d61b7d237952a2cf6686be88525c040da21699d4
soptak96 <144949460+soptak96@users.noreply.github.com>
2023-12-24 14:20:22 +0600

    finishing the js logic

--- script.js
+++ script.js
@@ -11,46 +11,124 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    return a/b;
+    if(b === 0) {
+        alert("You can not divide by 0!")
+        return null;
+    }
+    else return a/b;
 }
 
 let firstInput = "" ;
-let operator;
+let operator = null;
 let secondInput = "";
+let resetDisplay = false;
 
 
 
-function operate() {
+function operate(operator,a,b) {
+    a = Number(a);
+    b = Number(b);
+
     if(operator === "+"){
-       return add(firstInput,secondInput)
+       return add(a,b)
     }
-    if(operator === "-"){
-        return subtract(firstInput,secondInput)
+    if(operator === "−"){
+        return subtract(a,b)
     }
-    if(operator === "*"){
-        return multiply(firstInput,secondInput)
+    if(operator === "×"){
+        return multiply(a,b)
     }
-    if(operator === "/"){
-        return divide(firstInput,secondInput)
+    if(operator === "÷"){
+        return divide(a,b)
     }
 }
 
-const numberKeys = document.querySelectorAll("[data-number]")
-const Display = document.querySelector("#typing-display");
+const numberKeys = document.querySelectorAll("[data-number]");
+const operatorKeys = document.querySelectorAll("[data-operate")
+const currentDisplay = document.querySelector("#typing-display");
+const resultingDisplay = document.querySelector("#resulting-display");
+const equalButton = document.querySelector("#equal");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const pointButton = document.querySelector("#decemal")
 
-function displayNumber() {
-    firstInput += this.value;
-    Display.textContent = firstInput;
+function displayNumber(number) {
+    if(resetDisplay === true || currentDisplay.textContent === "0") 
+        resetCurrentDisplay();
+    currentDisplay.textContent += number;
+    }
+
+numberKeys.forEach(key => {
+    key.addEventListener("click", () => {displayNumber(key.innerText)});
+    })
+
+operatorKeys.forEach(keys => {
+    keys.addEventListener("click", () => setOperation(keys.innerText))
+})
+
+equalButton.addEventListener("click", calculate)
+
+clearButton.addEventListener("click", clear)
+
+deleteButton.addEventListener("click", deleteKey)
+
+pointButton.addEventListener("click", decemal)
+
+function setOperation(op) {
+    if(operator !== null) calculate(); 
+    firstInput = currentDisplay.textContent;
+    operator = op;
+    console.log(operator);
+    resultingDisplay.textContent = `${firstInput} ${operator}`;
+    resetDisplay = true;
 }
-numberKeys.forEach(button => {
-    button.addEventListener("click", (e) => {
-        firstInput += e.target.innerText;
-        console.log(firstInput);
-        if(Number(firstInput)){
-            Display.textContent = firstInput;
-        }
-    });
-    
 
+function resetCurrentDisplay() {
+    currentDisplay.textContent = "";
+    resetDisplay = false;
+}
+
+function calculate() {
+    if(operator === null || resetDisplay) return;
+    secondInput = currentDisplay.textContent;
+    resultingDisplay.textContent = `${firstInput} ${operator} ${secondInput} = `
+    currentDisplay.textContent = operate(operator,firstInput, secondInput);
+    operator = null;
+}
+
+function clear() {
+    currentDisplay.textContent = "0";
+    resultingDisplay.textContent = null;
+    firstInput = "";
+    secondInput = "";
+    operator = null;
+    resetDisplay = false;
+}
+
+function deleteKey() {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0,-1)
+}
+
+function decemal() {
+    if(resetDisplay) resetCurrentDisplay();
+
+    if(currentDisplay.textContent.includes(".")) return;
+   
+    if(currentDisplay.textContent === "")
+        currentDisplay.textContent = "0";
+     currentDisplay.textContent += ".";
+   
+}
+
+window.addEventListener("keydown", (e) => {
+    if(e.key >=0 || e.key <= 0) displayNumber(e.key);
+    else if(e.key === ".") decemal();
+    else if(e.key === "+") setOperation("+");
+    else if(e.key === "-") setOperation("−");
+    else if(e.key === "*") setOperation("×");
+    else if(e.key === "/") setOperation("÷");
+    else if(e.key === "=" || e.key === "Enter") calculate();
+    else if(e.key === "Backspace") deleteKey();
+    else if(e.key === "Escape") clear();    
 })
 
